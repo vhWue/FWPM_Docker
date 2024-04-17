d589121c816646993cc20a4030e3e9d49f7fa500
DielN <niklas.diel@study.thws.de>
2024-04-17 04:54:02 +0200

    Use MariaDB as DB

--- app/config/db.py
+++ app/config/db.py
@@ -2,8 +2,11 @@ from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-#URL_DATABASE='mariadb+pymysql://root:root@localhost:3306/testdb'
-URL_DATABASE='sqlite:///fuba.db'
+DB_USER = 'root'
+DB_PASSWORD = 'pwroot'
+DB_HOSTNAME = 'mariadb'
+DB_NAME = 'fuba'
+URL_DATABASE = f'mariadb+mysqldb://{DB_USER}:{DB_PASSWORD}@{DB_HOSTNAME}:3306/{DB_NAME}'
 
 engine = create_engine(URL_DATABASE, echo=True, pool_recycle=3600)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
--- db/fubaclub.sql
+++ db/fubaclub.sql
@@ -1,30 +1,30 @@
 -- Create a new database (if it doesn't exist already)
-CREATE DATABASE IF NOT EXISTS football_club_db;
+CREATE DATABASE IF NOT EXISTS fuba;
 
 -- Switch to use the newly created database
-USE football_club_db;
+USE fuba;
 
 -- Table for Football Clubs
 CREATE TABLE clubs (
-    club_id INT AUTO_INCREMENT PRIMARY KEY,
-    club_name VARCHAR(255) NOT NULL
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
 );
 
 -- Table for Players
 CREATE TABLE players (
-    player_id INT AUTO_INCREMENT PRIMARY KEY,
-    player_name VARCHAR(255) NOT NULL,
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
     club_id INT,
-    FOREIGN KEY (club_id) REFERENCES clubs(club_id)
+    FOREIGN KEY (club_id) REFERENCES clubs(id)
 );
 
 -- Example Data Insertion (Optional)
-INSERT INTO clubs (club_name) VALUES
+INSERT INTO clubs (name) VALUES
     ('Manchester United'),
     ('Real Madrid'),
     ('FC Barcelona');
 
-INSERT INTO players (player_name, club_id) VALUES
+INSERT INTO players (name, id) VALUES
     ('Cristiano Ronaldo', 2), -- Ronaldo plays for Real Madrid
     ('Lionel Messi', 3),       -- Messi plays for FC Barcelona
     ('Paul Pogba', 1);         -- Pogba plays for Manchester United
