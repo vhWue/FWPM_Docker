a65cf5835c574f7e541726e7cc0d2dc0d47f742a
DielN <niklas.diel@study.thws.de>
2024-04-17 05:01:46 +0200

    Remove port from DB URL

--- app/config/db.py
+++ app/config/db.py
@@ -6,7 +6,7 @@ DB_USER = 'root'
 DB_PASSWORD = 'pwroot'
 DB_HOSTNAME = 'mariadb'
 DB_NAME = 'fuba'
-URL_DATABASE = f'mariadb+mysqldb://{DB_USER}:{DB_PASSWORD}@{DB_HOSTNAME}:3306/{DB_NAME}'
+URL_DATABASE = f'mariadb+mysqldb://{DB_USER}:{DB_PASSWORD}@{DB_HOSTNAME}/{DB_NAME}'
 
 engine = create_engine(URL_DATABASE, echo=True, pool_recycle=3600)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
