7dcfa0a3cb95d78cd72b1e1387021a330b17227d
Hoppe, Viktor <viktor.hoppe@study.thws.de>
2024-04-25 16:59:49 +0200

    fix sql file error

--- db/fubaclub.sql
+++ db/fubaclub.sql
@@ -19,12 +19,12 @@ CREATE TABLE player (
 );
 
 -- Example Data Insertion (Optional)
-INSERT INTO clubs (name) VALUES
+INSERT INTO club (name) VALUES
     ('Manchester United'),
     ('Real Madrid'),
     ('FC Barcelona');
 
-INSERT INTO players (name, club_id) VALUES
+INSERT INTO player (name, club_id) VALUES
     ('Cristiano Ronaldo', 2), -- Ronaldo plays for Real Madrid
     ('Lionel Messi', 3),       -- Messi plays for FC Barcelona
     ('Paul Pogba', 1);         -- Pogba plays for Manchester United
