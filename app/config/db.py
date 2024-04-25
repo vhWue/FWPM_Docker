a18a0e5e49bb4b0a4af7f2253677065f64074216
AlexJFrank <alexjf@outlook.de>
2024-04-25 20:15:34 +0200

    db config adjustments to enable devcontainer use with sqlite db

--- app/config/db.py
+++ app/config/db.py
@@ -1,14 +1,14 @@
 from sqlmodel import Session, create_engine, SQLModel
+import os
 
 DB_USER = 'root'
 DB_PASSWORD = 'pwroot'
 DB_HOSTNAME = 'mariadb'
 DB_NAME = 'fuba'
-URL_DATABASE = f'mariadb+mysqldb://{DB_USER}:{DB_PASSWORD}@{DB_HOSTNAME}/{DB_NAME}'
+
+URL_DATABASE = "sqlite:///devfuba.db" if os.getenv('APP_ENV') == 'dev' else f'mariadb+mysqldb://{DB_USER}:{DB_PASSWORD}@{DB_HOSTNAME}/{DB_NAME}'
 
 engine = create_engine(URL_DATABASE, echo=True, pool_recycle=3600)
-SQLModel.metadata.create_all(engine)
-
 
 def get_session():
     with Session(engine) as session:
--- app/main.py
+++ app/main.py
@@ -1,12 +1,14 @@
 from fastapi import FastAPI
 
 from routes import club, player
+from config.db import SQLModel, engine
 
 app = FastAPI()
 
 app.include_router(club.router, prefix="/clubs", tags=["clubs"])
 app.include_router(player.router, prefix="/players", tags=["players"])
 
+SQLModel.metadata.create_all(engine)
 
 @app.get("/")
 def read_root():
